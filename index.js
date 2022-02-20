9ae31ef949deded523bd11bbff26deaa61e17063
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-02-20 14:06:23 -0600

    added necessary packages and questions array

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github user name?'
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What email address would you like to display on this README?'
+    }, 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project'
+    }, 
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How can a user install this software?'
+    }, 
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any additional pertinent user information.'
+    }, 
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please describe how a user can contribute to this repo.'
+    }, 
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can a user run tests with this software?'
+    }, 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
