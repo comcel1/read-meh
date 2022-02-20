1afb3cbce9e5d8f5dc6478e3f2ee3615fd19fc02
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-02-20 14:59:27 -0600

    added path to required packages; wrote functions to write README file and to initialize the app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -45,13 +46,20 @@ const questions = [
         name: 'tests',
         message: 'How can a user run tests with this software?'
     }, 
-];
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        writeToFile('README.MD', generateMarkdown({...responses}))
+    })
+}
+-
 
 // Function call to initialize app
 init();
