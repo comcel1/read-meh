34b23ccac1cfb2521cbf54c7f821d4c2fa8823c6
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-02-20 18:16:41 -0600

    added markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,7 +21,10 @@ renderLicenseBadge = license => {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
+renderLicenseLink = license => {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -36,8 +39,44 @@ function renderLicenseSection(license) {}
 
 function generateMarkdown(data) {
 return `# ${data.title}
-## license 
-${renderLicenseBadge(data.license)}`
+
+## Description
+${data.description}
+
+
+## Table of Contents
+* [Installation](#installation) \n
+* [Usage](#usage)  \n
+* [License](#license)  \n
+* [Contributors](#contributing)  \n
+* [Questions](#questions)  
+
+
+## Installation
+${data.installation}
+
+
+## Usage
+${data.usage}
+
+
+## Contributions
+${data.contributing}
+
+
+## Tests
+${data.tests}
+
+
+## License
+${renderLicenseBadge(data.license)}
+
+
+## Questions
+Please don't hesitate to reach out if you have any questions or comments. You can reach me through github or directly via email. 
+[Github](https://github.com/${data.github})  
+${data.email}
+`
 }
 
 module.exports = generateMarkdown;
